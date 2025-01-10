08e2aad157d6f44bb1daad21e9759cef11cfb3cc
Rene <renegiragosyan@gmail.com>
2025-01-10 22:44:41 +0400

    adding new files

---
+++ Udemy/Expressions/AreaOfCyrcle.py
@@ -0,0 +1,4 @@
+import math
+radius = float(input("Enter Raduis: "))
+area = math.pi * radius * radius
+print(f'area of cyrcul is {area}')
---
+++ Udemy/Expressions/CuboidCurfaceArea.py
@@ -0,0 +1,7 @@
+
+
+lenght = float(input('Enter lenght: '))
+width = float(input(' Enter widht: '))
+height = float(input('Enter height'))
+area = 2 * (lenght * width + width * height + lenght * height)
+print(f'Curface Area of cyrcle is {area}')
