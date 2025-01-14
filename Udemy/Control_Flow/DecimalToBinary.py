5aeb5942039809d3d6ecb1b4406fe828b0b2e570
Rene <renegiragosyan@gmail.com>
2025-01-14 22:07:43 +0400

    finishing decimal to bianry file

--- Udemy/Control_Flow/DecimalToBinary.py
+++ Udemy/Control_Flow/DecimalToBinary.py
@@ -1,14 +1,24 @@
-num = int(input("Enter number"))
-yerkuakan = 0
-
-while num > 0:
-    r = num % 2
-    num = num // 2
-    yerkuakan = yerkuakan * 10 + r
-print(yerkuakan)
-rev = 0
-while yerkuakan > 0:
-    r = yerkuakan % 10
-    yerkuakan = yerkuakan // 10
-    rev = rev * 10 + r
-print(rev)
+# num = int(input("Enter number"))
+# yerkuakan = 0
+# How to convert digit from Decimal to BIanry
+# while num > 0:
+#     r = num % 2
+#     num = num // 2
+#     yerkuakan = yerkuakan * 10 + r
+# print(yerkuakan)
+# How to make it Reversed
+# rev = 0
+# while yerkuakan > 0:
+#     r = yerkuakan % 10
+#     yerkuakan = yerkuakan // 10
+#     rev = rev * 10 + r
+# print(rev)
+#----------------------------------------------
+# this is the Correct code
+deci = int(input('enter deci num: '))
+binar = ''
+while deci > 0:
+    r = deci % 2
+    deci = deci // 2
+    binar = str(r) + binar
+print('bin', binar, 'binar')
