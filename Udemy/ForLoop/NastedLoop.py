27ff5d68fd169929731449218a7d2d27a1c31b63
Rene <renegiragosyan@gmail.com>
2025-01-20 14:15:57 +0400

    practise

--- Udemy/ForLoop/NastedLoop.py
+++ Udemy/ForLoop/NastedLoop.py
@@ -1,7 +1,27 @@
-x = ['x','i','z']
-m = ['a','b','c','d','e']
-for i in m:
+for i in range(0,5):
+    for j in range(0,5):
+        print('*', end=', ')
+    print('')
 
-    for j in x:
-        print(i, j, end=', ')
+print('')
+
+for i in range(0,5):
+    for j in range(0,5):
+        if i <= j:
+            print('*', end=', ')
+    print('')
+
+print('')
+
+for i in range(0,5):
+    for j in range(0,5):
+        if i >= j:
+            print('*', end=', ')
+    print('')
+print('')
+s1 ='abc'
+s2 = 'xyz'
+for i in s1:
+    for j in s2:
+        print(j, end=' ')
     print('')
